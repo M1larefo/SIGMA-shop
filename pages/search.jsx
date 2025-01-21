b701ad359c3d350ae59760193d25cd8b48e444a0
M1larefo <antonrogachevskiy112008@gmail.com>
2025-01-21 10:21:08 +0300

    Update search.jsx

--- pages/search.jsx
+++ pages/search.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useState, useEffect, useRef } from 'react';
 
+import Link from 'next/link';
 
-import { Link, animateScroll as scroll } from "react-scroll";
 
 
 
